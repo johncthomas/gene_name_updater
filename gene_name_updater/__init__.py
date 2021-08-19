4f55cf3fb1b5a729c5e3e1c789bb9ae91570489a
johnc.thomas <jct61@cam.ac.uk>
2021-08-19 16:25:27 +0100

    added function to do gene symbol update first by HGNC then NCBI

--- gene_name_updater/__init__.py
+++ gene_name_updater/__init__.py
@@ -1,2 +1,39 @@
 from .hgnc import hgnc_approved_symbol, symbol_ids_table
 from .ncbi import entrez_name_id
+import pandas as pd
+import numpy as np
+
+def map_and_ambig(gset:np.ndarray, nan_when_missing=True) -> dict:
+    """Returns a map of original names to udpated, array of ambiguous names,
+    and array of genes not found in HGNC or Entrez databases.
+
+    A dictionary is returned with keys:
+        'gene_series': pd.Series. indexed by gset with updated names (if found)
+            as values. Ambiguous queries will have a list of possible names.
+        'ambiguous': np.ndarray. Genes with multiple possible values according
+            to HGNC database.
+        'no_hits': Genes not found in either the HGNC or NCBI databases.
+
+    Final Series has original name when no other is found"""
+
+    if nan_when_missing:
+        initval = np.nan
+    else:
+        initval = gset
+
+    found = pd.Series(initval, index=gset).apply(hgnc_approved_symbol)
+    # # any nontargeting that have slipped through
+    # nts = list(map(is_ntctrl, gset))
+    # found.loc[nts] = 'Non-targeting'
+    ambiguous = found.loc[found.apply(lambda x: (type(x) is list) or (type(x) is np.ndarray))].index
+    print('Ambiguous:', len(ambiguous))
+    # try NCBI
+    nfm = found.isna()
+    print('Searching NCBI for:', nfm.sum())
+    found.loc[nfm] = found.loc[nfm].index.map(lambda g: entrez_name_id(g, null_value=np.nan)[0])
+    # take the raw array so we can apply it to the index directly
+    nfm = found.isna().values
+    print("No result for :", sum(nfm))
+    no_hits = found.index[nfm].values
+    found.loc[nfm] = found.index[nfm].values
+    return {'genes':found, 'ambiguous':ambiguous, 'no_hits':no_hits}
